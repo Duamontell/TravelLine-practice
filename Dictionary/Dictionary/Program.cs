726e37ffdb19eb6419331831c9ca4b44ee0767a8
Maksim <duamontell@gmail.com>
2024-07-09 16:22:58 +0300

    Изменил принцип работы словаря

--- Dictionary/Dictionary/Program.cs
+++ Dictionary/Dictionary/Program.cs
@@ -1,101 +1,111 @@
 ﻿Welcome();
 
 string dictionaryFile = "dictionary.txt";
-CheckDictionaryFile();
 Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+CheckDictionaryFile();
+LoadDictionaryFile();
 
 while ( true )
 {
     Console.Write( "Write a word to translate: " );
-    string userWord = Console.ReadLine();
+    string userWord = Console.ReadLine().ToLower();
+
     while ( string.IsNullOrWhiteSpace( userWord ) )
     {
         Console.Write( "Write a word to translate: " );
-        userWord = Console.ReadLine();
+        userWord = Console.ReadLine().ToLower();
     }
+
     string translateWord = TranslateWord( userWord );
+
     if ( translateWord != null )
     {
         Console.WriteLine( $"Translated word: {translateWord}" );
     }
     else
     {
-        Console.WriteLine( "That word is not found in the dictionary, would you like to add it?" );
-        PrintMenu();
-        string userCommand = Console.ReadLine();
-        switch ( userCommand )
-        {
-            case "1":
-                AddNewWord( userWord );
-                break;
-            case "2":
-                break;
-            default:
-                Console.WriteLine( "Unknown command" );
-                break;
-        }
+        WordNotFound( userWord );
     }
 }
 
 void Welcome()
 {
-    Console.WriteLine( "Dictionary" );
+    Console.WriteLine( "Dictionary" ); //Приветственное сообщение
+}
+
+void LoadDictionaryFile()
+{
+    foreach ( string line in File.ReadAllLines( dictionaryFile ) )
+    {
+        string[] word = line.Split( '=' );
+        dictionary[ word[ 0 ].Trim() ] = word[ 1 ].Trim();
+    }
 }
 
 void CheckDictionaryFile()
 {
     if ( !File.Exists( dictionaryFile ) )
     {
-        using ( File.Create( dictionaryFile ) )
-        {
+        using ( File.Create( dictionaryFile ) ) {}
 
-        }
         Console.WriteLine( "Create dictionary" ); //Словарь создался в папке bin/Debug/net8.0
     }
 }
 
 string TranslateWord( string word )
 {
-    using ( StreamReader file = new StreamReader( dictionaryFile ) )
+    if ( dictionary.TryGetValue( word, out string result ) )
     {
-        string line;
-        while ( ( line = file.ReadLine() ) != null )
-        {
-            string[] parts = line.Split( '=' ); //Делим строку на ключ и значение
-            string leftWord = parts[ 0 ];
-            string rightWord = parts[ 1 ];
-            dictionary[ leftWord ] = rightWord;
-            if ( leftWord == word ) //Ищем наше слово
-            {
-                return rightWord;
-            }
-            else if ( rightWord == word )
-            {
-                return leftWord;
-            }
-        }
+        return result; //Слово найдено в словаре
     }
-    return null; //Слово не найдено
+
+    return null; //Слово не найдено в словаре
 }
 
 void AddNewWord( string word )
 {
     Console.Write( "Write translate to this word: " );
-    string userWordTranslate = Console.ReadLine();
+    string userWordTranslate = Console.ReadLine().ToLower();
+
     while ( string.IsNullOrWhiteSpace( userWordTranslate ) )
     {
         Console.Write( "Write translate this word: " );
-        userWordTranslate = Console.ReadLine();
+        userWordTranslate = Console.ReadLine().ToLower();
     }
+
     using ( StreamWriter file = new StreamWriter( dictionaryFile, append: true ) )
+
     {
         file.WriteLine( $"{word}={userWordTranslate}" );
+        dictionary[ word ] = userWordTranslate;
     }
+
     Console.WriteLine( "New word added" );
 }
 
-void PrintMenu()
+void PrintAddMenu()
 {
     Console.WriteLine( "[1] Add word in dictionary" );
     Console.WriteLine( "[2] Not add" );
+}
+
+void WordNotFound( string word )
+{
+    Console.WriteLine( "That word is not found in the dictionary, would you like to add it?" );
+    PrintAddMenu();
+
+    string userCommand = Console.ReadLine();
+
+    switch ( userCommand )
+    {
+        case "1":
+            AddNewWord( word );
+            break;
+        case "2":
+            break;
+        default:
+            Console.WriteLine( "Unknown command" );
+            break;
+    }
 }
