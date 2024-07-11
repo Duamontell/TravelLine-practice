d1c91fad07e26541422facde19950ca526a4611e
Maksim <duamontell@gmail.com>
2024-07-11 14:15:34 +0300

    Добавил меню, выход из проги, оптимизировал работу с файлом и остальные небольшие правки

--- Dictionary/Dictionary/Program.cs
+++ Dictionary/Dictionary/Program.cs
@@ -1,27 +1,94 @@
-﻿Welcome();
+﻿Console.WriteLine( "Dictionary" );
 
 string dictionaryFile = "dictionary.txt";
+Dictionary<string, string> initDictionary = new Dictionary<string, string>();
 Dictionary<string, string> dictionary = new Dictionary<string, string>();
 
-CheckDictionaryFile();
 LoadDictionaryFile();
 
 while ( true )
 {
-    Console.Write( "Write a word to translate: " );
-    string userWord = Console.ReadLine().ToLower();
+    PrintMenu();
 
-    while ( string.IsNullOrWhiteSpace( userWord ) )
+    string userCommand = Console.ReadLine().Trim();
+
+    switch ( userCommand )
     {
-        Console.Write( "Write a word to translate: " );
-        userWord = Console.ReadLine().ToLower();
+        case "1":
+            TranslateWord();
+            break;
+        case "2":
+            AddNewWord();
+            break;
+        case "3":
+            SaveDictionary();
+            return;
+    }
+}
+
+void PrintMenu()
+{
+    Console.WriteLine( "[1] Translate word" );
+    Console.WriteLine( "[2] Add new word to dictionary" );
+    Console.WriteLine( "[3] Exit" );
+}
+
+void LoadDictionaryFile()
+{
+    if ( !File.Exists( dictionaryFile ) )
+    {
+        using ( File.Create( dictionaryFile ) )
+        { }
+        Console.WriteLine( "Create dictionary" );
     }
 
-    string translateWord = TranslateWord( userWord );
-
-    if ( translateWord != null )
+    foreach ( string line in File.ReadAllLines( dictionaryFile ) )
     {
-        Console.WriteLine( $"Translated word: {translateWord}" );
+        string[] word = line.Split( '=' );
+        dictionary[ word[ 0 ] ] = word[ 1 ];
+    }
+}
+
+void AddNewWord()
+{
+    Console.Write( "Write a word for translate: " );
+    string userWord = Console.ReadLine().ToLower().Trim();
+
+    while ( string.IsNullOrWhiteSpace( userWord ) || IsString( userWord ) )
+    {
+        Console.Write( "Invalid input, write a word for translate: " );
+        userWord = Console.ReadLine().ToLower().Trim();
+    }
+
+    Console.Write( "Write translate to this word: " );
+    string userWordTranslate = Console.ReadLine().ToLower().Trim();
+
+    while ( string.IsNullOrWhiteSpace( userWordTranslate ) || IsString( userWordTranslate ) )
+    {
+        Console.Write( "Invalid input, write translate this word: " );
+        userWordTranslate = Console.ReadLine().ToLower().Trim();
+    }
+
+    initDictionary[ userWord ] = userWordTranslate;
+    dictionary[ userWord ] = userWordTranslate;
+    Console.WriteLine( "New word added" );
+}
+
+void TranslateWord()
+{
+    Console.Write( "Write a word to translate: " );
+    string userWord = Console.ReadLine().ToLower().Trim();
+
+    while ( string.IsNullOrWhiteSpace( userWord ) || IsString( userWord ) )
+    {
+        Console.Write( "Invalid input, write a word to translate: " );
+        userWord = Console.ReadLine().ToLower().Trim();
+    }
+
+    if ( dictionary.TryGetValue( userWord, out string result ) )
+    {
+        Console.WriteLine( $"Translated word: {result}" );
+        Console.WriteLine();
     }
     else
     {
@@ -29,58 +96,19 @@ while ( true )
     }
 }
 
-void Welcome()
-{
-    Console.WriteLine( "Dictionary" ); //Приветственное сообщение
-}
-
-void LoadDictionaryFile()
-{
-    foreach ( string line in File.ReadAllLines( dictionaryFile ) )
-    {
-        string[] word = line.Split( '=' );
-        dictionary[ word[ 0 ].Trim() ] = word[ 1 ].Trim();
-    }
-}
-
-void CheckDictionaryFile()
-{
-    if ( !File.Exists( dictionaryFile ) )
-    {
-        using ( File.Create( dictionaryFile ) ) {}
-
-        Console.WriteLine( "Create dictionary" ); //Словарь создался в папке bin/Debug/net8.0
-    }
-}
-
-string TranslateWord( string word )
-{
-    if ( dictionary.TryGetValue( word, out string result ) )
-    {
-        return result; //Слово найдено в словаре
-    }
-
-    return null; //Слово не найдено в словаре
-}
-
-void AddNewWord( string word )
+void AddTranslateWord( string word )
 {
     Console.Write( "Write translate to this word: " );
-    string userWordTranslate = Console.ReadLine().ToLower();
+    string userWordTranslate = Console.ReadLine().ToLower().Trim();
 
-    while ( string.IsNullOrWhiteSpace( userWordTranslate ) )
+    while ( string.IsNullOrWhiteSpace( userWordTranslate ) || IsString( userWordTranslate ) )
     {
-        Console.Write( "Write translate this word: " );
-        userWordTranslate = Console.ReadLine().ToLower();
+        Console.Write( "Invalid input, write translate this word: " );
+        userWordTranslate = Console.ReadLine().ToLower().Trim();
     }
 
-    using ( StreamWriter file = new StreamWriter( dictionaryFile, append: true ) )
-
-    {
-        file.WriteLine( $"{word}={userWordTranslate}" );
-        dictionary[ word ] = userWordTranslate;
-    }
-
+    initDictionary[ word ] = userWordTranslate;
+    dictionary[ word ] = userWordTranslate;
     Console.WriteLine( "New word added" );
 }
 
@@ -95,17 +123,41 @@ void WordNotFound( string word )
     Console.WriteLine( "That word is not found in the dictionary, would you like to add it?" );
     PrintAddMenu();
 
-    string userCommand = Console.ReadLine();
+    string userCommand = Console.ReadLine().Trim();
 
     switch ( userCommand )
     {
         case "1":
-            AddNewWord( word );
+            AddTranslateWord( word );
             break;
         case "2":
+            Console.WriteLine();
             break;
         default:
             Console.WriteLine( "Unknown command" );
             break;
+    }
+}
+
+bool IsString( string word )
+{
+    if ( int.TryParse( word, out _ ) )
+    {
+        return true;
+    }
+    return false;
+}
+
+void SaveDictionary()
+{
+    if ( initDictionary.Count != 0 )
+    {
+        using ( StreamWriter file = File.AppendText( dictionaryFile ) )
+        {
+            foreach ( KeyValuePair<string, string> word in initDictionary )
+            {
+                file.WriteLine( $"{word.Key}={word.Value}" );
+            }
+        }
     }
 }
