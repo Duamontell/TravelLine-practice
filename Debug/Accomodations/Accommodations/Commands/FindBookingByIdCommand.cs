c52448749f24c6150263eab656a7bedd527ea716
Maksim <duamontell@gmail.com>
2024-07-17 18:23:27 +0300

    Исправил баги

---
+++ Debug/Accomodations/Accommodations/Commands/FindBookingByIdCommand.cs
@@ -0,0 +1,21 @@
+using Accommodations.Models;
+
+namespace Accommodations.Commands;
+
+public class FindBookingByIdCommand( IBookingService bookingService, Guid bookingId ) : ICommand
+{
+    public void Execute()
+    {
+        Booking? booking = bookingService.FindBookingById( bookingId );
+
+        //Поправил вывод названия категории
+        Console.WriteLine( booking != null
+            ? $"Booking found: {booking.RoomCategory.Name} for User {booking.UserId}"
+            : "Booking not found." );
+    }
+
+    public void Undo()
+    {
+        Console.WriteLine( $"Undo operation is not supported for {nameof( GetType )}." );
+    }
+}
